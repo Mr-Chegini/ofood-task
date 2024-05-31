95f033a65dca658e085b3a3dc9d55075154fec90
Chegini <mr.chegini007@gmail.com>
2024-05-31 14:23:17 +0330

    Add feedback resource

---
+++ src/feedbacks/entities/feedback.entity.ts
@@ -0,0 +1,31 @@
+import { FeedbackType } from 'src/enums/feedback-types.enum';
+import { Order } from 'src/orders/entities/order.entity';
+import { Store } from 'src/stores/entities/store.entity';
+import {
+  Entity,
+  Column,
+  BaseEntity,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+} from 'typeorm';
+
+@Entity()
+export class Feedback extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => Order)
+  order_id: Order;
+
+  @ManyToOne(() => Store)
+  store_id: Store;
+
+  @Column({ type: 'enum' })
+  type: FeedbackType;
+
+  @Column({ type: 'int2' })
+  rating: number;
+
+  @Column()
+  comment: string;
+}
